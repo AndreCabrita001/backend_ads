470f7c42487cfdb037c9011ccf97363aa602f626
AndreCabrita001 <84391133+AndreCabrita001@users.noreply.github.com>
2021-10-23 17:28:18 +0100

    Config deploy Test4

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-
+const Ad = require("./models/Ad");
 const PORT = process.env.PORT || 3000;
 
 const adGetRoute = require("./routes/adsget");
